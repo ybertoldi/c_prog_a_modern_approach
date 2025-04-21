ef11646065c1b39fe335f2794b82e11d63bda855
ybertoldi <yuri.bertoldi003@gmail.com>
2025-04-20 22:15:59 -0300

    correcao

--- chap08/prog2_hand_of_cards.c
+++ chap08/prog2_hand_of_cards.c
@@ -1,19 +1,28 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-  const char *ranks[] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+#define NUM_SUITS 4
+#define NUM_RANKS 13
+
+int main() {
+  const char *ranks[] = {"A", "2", "3",  "4", "5", "6", "7",
+                         "8", "9", "10", "J", "Q", "K"};
   const char *suits[] = {"c", "d", "h", "s"};
-  int n;
+  bool inhand[NUM_SUITS][NUM_RANKS] = {false};
+  int n, handr, hands;
 
   printf("enter the number of cards in hand: ");
   scanf("%d", &n);
 
   printf("Your hand: ");
   while (--n > 0) {
-      int handr = random() % 13;
-      int hands = rand() % 4;
-      printf("%s%s ", ranks[handr], suits[hands]);
+    do {
+      handr = random() % 13;
+      hands = rand() % 4;
+    } while (inhand[hands][handr]);
+
+    printf("%s%s ", ranks[handr], suits[hands]);
   }
   printf("\n");
 
